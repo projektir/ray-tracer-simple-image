5cbd705813a2826d06b9d8c403a05c8216d2facf
projektir <oprojektir@gmail.com>
2017-05-21 00:01:54 -0400

    Renaming some of scene parsing

--- src/parser.rs
+++ src/parser.rs
@@ -10,23 +10,21 @@ use scene::Scene;
 use shape::sphere::Sphere;
 
 pub fn load_scene(path: &str) -> Scene {
-    let scenes_path = Path::new(path);
+    let path = Path::new(path);
 
     let mut file: File;
     
-    file = match File::open(scenes_path) {
+    file = match File::open(path) {
         Ok(file) => file,
         Err(_) => panic!("Unable to find file {:?}, make sure you are in the root directory of the project",
-            scenes_path),
+            path),
     };
 
-    let mut scene_json: String = String::new();
+    let mut scene_string: String = String::new();
+    file.read_to_string(&mut scene_string).unwrap();
 
-    file.read_to_string(&mut scene_json).unwrap();
-
-    let de_scene: serde_json::Value = serde_json::from_str(&scene_json).unwrap();
-
-    let scene_array = de_scene.as_array().unwrap();
+    let scene_json: serde_json::Value = serde_json::from_str(&scene_string).unwrap();
+    let scene_array = scene_json.as_array().unwrap();
 
     let mut scene = Scene::new();
 
@@ -44,6 +42,8 @@ pub fn render_scene(scene: Scene, width: u32, height: u32) {
     let mut image_vec = vec![0; 4 * (width * height) as usize];
 
     let img: RgbaImage = ImageBuffer::from_raw(width, height, image_vec).unwrap();
+
+
 
     let ref mut fout = File::create(&Path::new("test.png")).unwrap();
     let _ = save_buffer("test.png", &img, width, height, ColorType::RGBA(8)).unwrap();
