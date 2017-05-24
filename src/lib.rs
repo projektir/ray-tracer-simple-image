e63982b386f363ebbdfad08f0775286303090da9
projektir <oprojektir@gmail.com>
2017-05-23 23:10:46 -0400

    Use more idiomatic order

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+extern crate image;
+
 pub mod lin_alg;
 pub mod scene;
 pub mod shape;
 
 pub mod parser;
 pub mod tracer;
-
-#[macro_use]
-extern crate serde_derive;
-extern crate serde_json;
-
-extern crate image;
